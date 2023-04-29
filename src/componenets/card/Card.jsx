bea7941fbcbef7c6e75ff503c5181c81ff8a9d42
ocnm <95259793+ocnm@users.noreply.github.com>
2023-04-29 15:08:01 -0600

    styling

--- src/componenets/card/Card.jsx
+++ src/componenets/card/Card.jsx
@@ -2,27 +2,32 @@ import React from 'react'
 import styles from './Card.module.scss'
 import { useDrag } from 'react-dnd'
 import { useSelector } from 'react-redux'
+import { ListItem } from '@mui/material'
+import { cardStyles } from '../../styles/localStyles'
 
-const Card = ({info, id}) => {
+const CardItem = ({ info, id }) => {
 
   const visibleProjectIndex = useSelector(state => state.projects.visibleProjectIndex)
 
-  const [{isDragging}, drag] = useDrag(() => ({
-    type: 'div',  
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: 'div',
 
     //the drop in useDrop can using this name 'item' to access the data
-    item: { taskInfo: info, taskId: id, projectIndex: visibleProjectIndex},
+    item: { taskInfo: info, taskId: id, projectIndex: visibleProjectIndex },
     //this i think is to check if the element is being dragged right now
     collect: (monitor) => ({
-      isDragging:  !!monitor.isDragging(),
+      isDragging: !!monitor.isDragging(),
     }),
   }))
 
+  const {ListItemStyled,} = cardStyles;
+
   return (
-    <div ref={drag} className={styles.card}>
-      {info}
-    </div>
+    <ListItemStyled ref={drag}>
+        {info} <br />
+      Some other stuff
+    </ListItemStyled>
   )
 }
 
-export default Card
+export default CardItem
--- src/componenets/state/State.jsx
+++ src/componenets/state/State.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import styles from './State.module.scss'
-import Card from '../card/Card'
+import CardItem from '../card/Card'
+import { stateStyles } from '../../styles/localStyles'
+import { Typography, List } from '@mui/material';
+import PlaylistAddCheckCircleRoundedIcon from '@mui/icons-material/PlaylistAddCheckCircleRounded';
 
 // state here means what level the task is at - to do, doing or done (it is a coloumn component)
 
-function State({type, list, ref1}) {
+function State({ type, list, ref1 }) {
+  const { BoxStyled } = stateStyles;
   return (
-    <div ref={ref1} className={styles.state}>
-      <h3>{type}</h3>
-      <ul>
-        {list.map(task => <li key={task.id}><Card info={task.info} id={task.id} /></li> )}
-      </ul>
-    </div>
+    <BoxStyled ref={ref1} className={styles.state}>
+      <Typography variant='h3'><PlaylistAddCheckCircleRoundedIcon/>{type}</Typography>
+      <List>
+        {list.map(task => <CardItem key={task.id} info={task.info} id={task.id} />)}
+      </List>
+    </BoxStyled>
   )
 }
 
