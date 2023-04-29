3e7d293c912df7275e3b3bd233aae50c9dd7b729
Anmol <anmolsingh1468@gmail.com>
2023-04-29 14:53:48 -0600

    changed add task process

--- src/componenets/card/Card.jsx
+++ src/componenets/card/Card.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Card.module.scss'
 import { useDrag } from 'react-dnd'
 import { useSelector } from 'react-redux'
 
-const Card = ({info, id}) => {
+const Card = ({info, id, description}) => {
+
+  const [showPopUp, setShowPopUp] = useState(false)
+
+  const togglePopUp = () => {
+    setShowPopUp(!showPopUp)
+  }
 
   const visibleProjectIndex = useSelector(state => state.projects.visibleProjectIndex)
 
@@ -11,7 +17,9 @@ const Card = ({info, id}) => {
     type: 'div',  
 
     //the drop in useDrop can using this name 'item' to access the data
-    item: { taskInfo: info, taskId: id, projectIndex: visibleProjectIndex},
+    item: { taskInfo: info, taskId: id, projectIndex: visibleProjectIndex,
+    taskDescription: description
+    },
     //this i think is to check if the element is being dragged right now
     collect: (monitor) => ({
       isDragging:  !!monitor.isDragging(),
@@ -19,8 +27,14 @@ const Card = ({info, id}) => {
   }))
 
   return (
-    <div ref={drag} className={styles.card}>
-      {info}
+    <div ref={drag} className={styles.card} onClick={togglePopUp}>
+      <h5>{info}</h5>
+      <div className={styles.banner} 
+      style={{display: showPopUp? 'block': 'none' }}
+      >
+        <h5>{info}</h5>
+        <p>{description}</p>
+      </div>
     </div>
   )
 }
